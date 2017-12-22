799f32e95f0e5ad434d8c2d05f4f9579f3cee45d
EC2 Default User <ec2-user@ip-172-31-14-135.us-east-2.compute.internal>
2017-12-22 19:01:28 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks=Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -24,11 +25,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    set_message
 
     if @task.update(message_params)
       flash[:success] = 'タスク は正常に更新されました'
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'タスク は正常に削除されました'
@@ -48,6 +47,10 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_message
+    @task = Task.find(params[:id])
+  end
 
   # Strong Parameter
   def message_params
